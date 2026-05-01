9ab1ceeabd2ad3f26047203bb998fd21c122436a
Gaurav <gauravg77061@gmail.com>
2026-05-01 18:18:32 +0530

    login and logout api

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -49,3 +49,54 @@ authRouter.post('signup',async(req,res)=>{
 
 
 })
+
+//login 
+
+authRouter.post('/login',async(req,res)=>{
+
+    try {
+        
+        const {email,password}=req.body;
+
+        if(!validator.isEmail(email)){
+            throw new Error("Invalid EMail id");
+            
+        }
+
+        const user=await User.findOne({email})
+
+        if(!user){
+            throw new Error("Invald email id")
+        }
+
+        //validating password 
+
+        const isPassword=await User.validatePassword(password)
+
+        if(isPassword){
+            const token=await user.getJwt()
+
+            res.cookie("token",token);
+
+            res.send(user);
+        }
+
+    } catch (error) {
+        throw new Error("Error"+error.message)
+    }
+
+})
+
+authRouter.post('/logout',async(req,res)=>{
+       try {
+
+        //expiring cookie changing date to date now
+
+        res.cookie("token",null,{
+        expires:new Date(Date.now()),
+    });
+    res.send("Logout successfully");
+    } catch (error) {
+        res.status(400).send("error "+ error.message);
+    }
+})
