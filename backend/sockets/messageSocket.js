bcea03ecdeecc327d62902bec343e9c026f29455
Gaurav <gauravg77061@gmail.com>
2026-05-10 01:43:58 +0530

    room created and able to send the message inside the room

--- backend/sockets/messageSocket.js
+++ backend/sockets/messageSocket.js
@@ -1,3 +1,6 @@
+const Group=require('../models/group')
+const Message=require("../models/message")
+
 const registerMessageSocket=(io,socket) =>{
     console.log("Socket connected",socket.id)
 
@@ -8,11 +11,49 @@ const registerMessageSocket=(io,socket) =>{
 
     })
 
+    //send message 
+    socket.on("send_message",async(data) => {
+        try {
+            const{groupId,senderId,text} = data;
+
+            if(!groupId || !senderId || !text){
+                throw new Error("Invald message data")
+
+            }
+
+            const group=await Group.findById(groupId);
+
+            if(!group){
+                throw new Error("Group not found")
+            }
+
+            const isMember = group.members.some(
+                (member) => member.toString() === senderId.toString()
+            );
+
+            if(!isMember){
+                throw new Error("you are not a member of this group")
+            }
+
+            const savedMessage=await Message.create({
+                groupId,
+                senderId,
+                role:"user",
+                text,
+            })
+
+            io.to(groupId).emit("receive_message",savedMessage)
+
+        } catch (error) {
+            console.log(error.message)
+        }
+    })
+
     socket.on("disconnect",()=>{
         console.log("Socket disconnected:",socket.id)
     });
 
-   
+    
 };
 
 module.exports=registerMessageSocket
--- backend/testSocket.js
+++ backend/testSocket.js
@@ -6,6 +6,20 @@ socket.on("connect", () => {
 
     console.log("Connected:", socket.id);
 
-    socket.emit("join_group", "12345");
+    const groupId = "69f5bc634498891224ab36fb";
+
+    socket.emit("join_group", groupId);
+
+    socket.emit("send_message", {
+        groupId,
+        senderId: "69f5d009c38faa7fc47cbb8e",
+        text: "Hello realtime group chat",
+    });
+
+});
+
+socket.on("receive_message", (message) => {
+
+    console.log("New Message:", message);
 
 });
